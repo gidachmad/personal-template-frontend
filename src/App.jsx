a67f94f4eaf2cc6894059fc6fb43431785b3c27b
Gid AAF <gaaf0809@gmail.com>
2023-01-08 22:48:37 +0700

    setting up tailwind and base page

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,20 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='min-h-screen bg-slate-800 text-slate-400 flex flex-col justify-center items-center space-y-10'>
+      <div className='w-10 aspect-square'>
+        <img src={reactLogo} alt='React Logo SPEEN' className='animate-spin' />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className='text-3xl font-bold underline'>
+        Lets count! the count is <span className='text-5xl'>{count}</span>
+      </h1>
+      <button className='px-4 py-2 border' onClick={() => setCount(count + 1)}>
+        +1
+      </button>
     </div>
   )
 }
